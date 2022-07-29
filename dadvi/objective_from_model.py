ffbeca9287d71c0fcd05cce88112def14507e7e2
Martin Ingram <martin.ingram@gmail.com>
2022-07-29 07:36:52 +0200

    Objective from log posterior works now

--- dadvi/objective_from_model.py
+++ dadvi/objective_from_model.py
@@ -1,8 +1,9 @@
 import numpy as np
-from .core import get_dadvi_draws
+from .core import get_dadvi_draws, DADVIFuns
 
 
-def get_objective_fun(log_posterior_fun, log_posterior_grad_fun):
+def build_dadvi_funs(log_posterior_fun, log_posterior_grad_fun):
+    # TODO: Optionally add hvp?
 
     def kl_est_fun(var_params, zs):
 
@@ -14,11 +15,11 @@ def get_objective_fun(log_posterior_fun, log_posterior_grad_fun):
 
         mean_log_prob = np.mean(log_probs)
 
-        scaled_zs = zs * np.exp(log_sds).reshape(-1, 1)
+        scaled_zs = zs * np.exp(log_sds).reshape(1, -1)
         grad_log_sd = np.mean(scaled_zs * grads, axis=0) + 1
         entropy = np.sum(log_sds)
         grad_of_mean = np.mean(grads, axis=0)
 
         return -(mean_log_prob + entropy), np.concatenate([-grad_of_mean, -grad_log_sd])
 
-    return kl_est_fun
+    return DADVIFuns(kl_est_and_grad_fun=kl_est_fun, kl_est_hvp_fun=None)
